3dbf8a67bec371442270ae3d8e4d11f333b5e223
Qianli Han <q36han@uwaterloo.ca>
2021-06-07 22:45:27 +0800

    final

--- solve.py
+++ solve.py
@@ -116,7 +116,7 @@ def get_successors(state):
                 new_state = copy_state(state, index, i)
                 successor.append(new_state)
                 i -=1
-            i = car.var_coord + car.length
+            i = car.var_coord + car.length -1
             while i <state.board.size and state.board.grid[car.fix_coord][i] == '.':
                 new_state = copy_state(state, index, i)
                 successor.append(new_state)
